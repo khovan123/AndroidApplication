3e0c374b8000505d78b2c915537a9fff0bf38cb8
khovan <minhpnq1807@gmail.com>
2024-03-16 18:52:53 +0700

    push demo MVVM for newbie

---
+++ CounterMVVM/app/src/main/java/com/tutorial/countermvvm/CounterModel.kt
@@ -0,0 +1,18 @@
+package com.tutorial.countermvvm
+
+data class CounterModel(var count : Int)
+
+class CounterRepository(){
+    private var _counter = CounterModel(0)
+
+    fun getCounter() = _counter
+
+    fun incrementCounter(){
+        _counter.count++
+    }
+
+    fun decrementCounter(){
+        _counter.count--
+    }
+
+}
